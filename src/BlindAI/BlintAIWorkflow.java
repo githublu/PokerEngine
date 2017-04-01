44bae182682ebe79a22cd7b951c0c65128500a66
Yi Lu <yilu@Yis-MacBook-Pro.local>
2017-04-01 12:58:34 -0700

    initial change to make blind AI changes needed for game workflow in master pkg

---
+++ src/BlindAI/BlintAIWorkflow.java
@@ -0,0 +1,18 @@
+package BlindAI;
+
+import master.Move;
+import master.TableInfo;
+
+public class BlintAIWorkflow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+	
+	private static Move PreflopMove(TableInfo tableInfo)
+	{
+		Move move = new Move();
+		return move;
+	}
+
+}
